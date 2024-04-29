5ff8bd10e8037bebbb511e3191eaff5baa6c514f
Patrick d'Harumi <166030449+PatrickHarumi@users.noreply.github.com>
2024-04-29 18:42:09 +0900

    小数点以下まるめ機能の実装

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ function effectiveDamage(power, armor, armorPenetration) {
   let effectiveArmor = normalize(armor) - normalize(armorPenetration);
   effectiveArmor = effectiveArmor <= 0 ? 0 : effectiveArmor;
   const damageDecrease = effectiveArmor / (100 + effectiveArmor);
-  return Math.floor(normalize(power) * (1 - damageDecrease));
+//  return Math.floor(normalize(power) * (1 - damageDecrease));
+  return Math.round(normalize(power) * (1 - damageDecrease));
 }
 
 /**
@@ -29,7 +30,7 @@ function effectiveDamage(power, armor, armorPenetration) {
 function normalize(n) {
   if (n < 0) {
     return 0;
-  } else if (n >= 2000) {
+  } else if (2000 <= n) {
     return 2000;
   } else {
     return n;
